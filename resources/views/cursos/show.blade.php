c0c88af58e360488c3385c23e941c0e067712c06
Jhon Fabio Cardona Martinez <fabio.cardona@coytex.com.co>
2021-06-05 01:48:37 -0500

    eliminado registro

--- resources/views/cursos/show.blade.php
+++ resources/views/cursos/show.blade.php
@@ -3,10 +3,15 @@
 @section('title', 'Crear curso')
 
 @section('content')
-    <h1>Bienvenido al curso de: {{ $curso->name }}</h1>    
+    <h1>Bienvenido al curso de: {{ $curso->name }}</h1>
     <a href="{{ route('cursos.index') }}">Volver a lista de cursos</a>
     <br>
-    <a href="{{ route('cursos.edit',$curso) }}">Editar curso</a>
-    <p><strong>Categoría: {{$curso->categoria}}</strong></p>
-    <p>{{$curso->description}}</p>
+    <a href="{{ route('cursos.edit', $curso) }}">Editar curso</a>
+    <p><strong>Categoría: {{ $curso->categoria }}</strong></p>
+    <p>{{ $curso->description }}</p>
+    <form action="{{ route('cursos.destroy', $curso) }}" method="post">
+        @csrf
+        @method('delete')
+        <button onclick="return(confirm('Deseas eliminar este registro ? '))">Eliminar</button>
+    </form>
 @endsection
