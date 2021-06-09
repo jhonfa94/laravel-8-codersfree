299238217491fcf3eeeb9f81d482585efc3f1463
Jhon Fabio Cardona Martinez <fabio.cardona@coytex.com.co>
2021-06-09 12:00:25 -0500

    Ruta resource, recursiva

--- routes/web.php
+++ routes/web.php
@@ -25,11 +25,13 @@ use App\Http\Controllers\CursoController;
 Route::get('/', HomeController::class); # Busca el método invoke
 
 
-Route::get('cursos', [CursoController::class, 'index'])->name('cursos.index');
+/* Route::get('cursos', [CursoController::class, 'index'])->name('cursos.index');
 Route::get('cursos/create', [CursoController::class, 'create'])->name('cursos.create');
 Route::post('cursos', [CursoController::class, 'store'])->name('cursos.store');
 // Route::get('cursos/{curso}', [CursoController::class, 'show'])->name('cursos.show');
 Route::get('cursos/{id}', [CursoController::class, 'show'])->name('cursos.show');
 Route::get('cursos/{curso}/edit', [CursoController::class, 'edit'])->name('cursos.edit');
 Route::put('cursos/{curso}', [CursoController::class, 'update'])->name('cursos.update');
-Route::delete('cursos/{curso}', [CursoController::class, 'destroy'])->name('cursos.destroy');
+Route::delete('cursos/{curso}', [CursoController::class, 'destroy'])->name('cursos.destroy'); */
+
+Route::resource('cursos',CursoController::class);
